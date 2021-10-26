592b3adf16a42dd2bdce2bcddf7a569fbd036abd
Varsha Jain <csvarshajain@gmail.com>
2021-10-25 22:19:41 -0400

    added heroku

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const { animals } = require('./data/animals');
 const express = require('express');
+const PORT = process.env.PORT || 3001;
 const app = express();
 
 
@@ -52,6 +53,6 @@ app.get('/api/animals', (req, res) => {
 });
 
 //1st step
-app.listen(3001, () => {
-    console.log(`API server now on port 3001!`);
+app.listen(PORT, () => {
+    console.log(`API server now on port ${PORT}!`);
 });
